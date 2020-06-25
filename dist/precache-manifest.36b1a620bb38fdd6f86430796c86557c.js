a250ad1032481c3245b2b8948d1d56ed7790d708
Yasmina Ablaeva <61462849+yasminaablaeva@users.noreply.github.com>
2020-06-25 21:13:44 +0500

    Add files via upload

---
+++ dist/precache-manifest.36b1a620bb38fdd6f86430796c86557c.js
@@ -0,0 +1,21 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d36af0d7c8516ca2310d85f49cc514c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "70a39ca80d3873e0e916",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "70a39ca80d3873e0e916",
+    "url": "main.css"
+  },
+  {
+    "url": "precache-manifest.8c76de8d48aad45fca595b96b3adc4fd.js"
+  },
+  {
+    "revision": "dcd789642634790851c2009af55798c3",
+    "url": "service-worker.js"
+  }
+]);
---
+++ dist/precache-manifest.8c76de8d48aad45fca595b96b3adc4fd.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a39ca80d3873e0e916",
+    "url": "bundle.min.js"
+  },
+  {
+    "revision": "70a39ca80d3873e0e916",
+    "url": "main.css"
+  }
+]);
